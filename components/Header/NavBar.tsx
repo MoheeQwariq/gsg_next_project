e62e794e2ef6093891d51d8d1042d71de7af7ce2
Faisal <zkryfysl@gmail.com>
2025-03-26 13:01:49 +0200

    insted of using hr , add border bottom to the nav

--- components/Header/NavBar.tsx
+++ components/Header/NavBar.tsx
@@ -8,8 +8,8 @@ import { CiLogout } from "react-icons/ci";
 
 const NavBar = () => {
   return (
-    <header className="sticky top-0 z-10  backdrop-blur-sm bg-[#EFEFEF]">
-      <div className="container mx-auto flex items-center justify-between border-b border-gray-200 px-4 py-4">
+    <header className="sticky top-0 z-10  backdrop-blur-sm bg-[#EFEFEF] ">
+      <div className="container mx-auto flex items-center justify-between border-b border-gray-200 px-4 py-4 border-b-gray-900">
         {/* عنوان الموقع */}
         <h1 className="text-2xl font-bold">
           <span className="bg-gradient-to-r from-[#7851E9] via-[#423ECD] to-[#3652E1] bg-clip-text text-transparent">
