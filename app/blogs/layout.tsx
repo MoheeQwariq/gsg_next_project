80b56afacb963bfc98299b547e011a2fb65ca399
Faisal <zkryfysl@gmail.com>
2025-03-26 05:03:46 -0400

    transfer the sticky side bar to the layout to be sharable with children page

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -1,7 +1,20 @@
+import TrendingBlogs from "@/components/blog/TrendingBlogs";
 import React from "react";
-
-const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+import TopPuplishers from "@/components/blog/TopPuplishers";
+const Bloglayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-4">
+        <div className="order-2 lg:order-1 lg:col-span-3">{children}</div>
+        <div className="order-1 lg:order-2 lg:sticky lg:top-24 lg:self-start">
+          <div className="space-y-6 ">
+            <TrendingBlogs />
+            <TopPuplishers />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default layout;
+export default Bloglayout;
--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,18 +1,16 @@
 import CardBlog from "@/components/blog/CardBlog";
 import Categories from "@/components/blog/Categories";
-import TopPuplishers from "@/components/blog/TopPuplishers";
-import TrendingBlogs from "@/components/blog/TrendingBlogs";
 import React from "react";
 import { FaPlus, FaSearch } from "react-icons/fa";
 
 const page = () => {
   return (
-    <div className="container mx-auto px-4 py-8 ">
-      <div className="flex justify-between items-center mb-6 pb-4">
+    <>
+      <div className="mb-6 flex flex-col justify-between gap-4 border-b border-gray-200 pb-4 sm:flex-row sm:items-center">
         <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#3652E1] to-[#8057F5]">
           المقالات
         </h1>
-        <button className="flex items-center gap-2 rounded-lg bg-gradient-to-r from-blue-600 to-indigo-600 px-5 py-2.5 text-sm font-medium text-white shadow-md transition duration-300 hover:from-blue-700 hover:to-indigo-700 hover:shadow-lg">
+        <button className="flex items-center gap-2 rounded-lg bg-gradient-to-r from-blue-600 to-indigo-600 px-5 py-2.5 text-sm font-medium text-white shadow-md transition duration-300 hover:from-blue-700 hover:to-indigo-700 hover:shadow-lg cursor-pointer">
           <FaPlus className="w-4 h-4" />
           <span>أضف مقال</span>
         </button>
@@ -33,28 +31,19 @@ const page = () => {
         {/* مجموعة تصنيفات المقالات */}
         <Categories />
       </div>
-
-      <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
-        {/* مجموعة المقالات  */}
-        <div className="space-y-6 lg:col-span-2">
-          <CardBlog />
-          <CardBlog />
-          <CardBlog />
-          <CardBlog />
-          <CardBlog />
-          <CardBlog />
-          <CardBlog />
-          <CardBlog />
-          <CardBlog />
-        </div>
-
-        {/* المقالات الرائجة */}
-        <div className="lg:sticky lg:top-24 lg:self-start">
-          <TrendingBlogs />
-          <TopPuplishers />
-        </div>
+      {/* مجموعة المقالات  */}
+      <div className="space-y-6 ">
+        <CardBlog />
+        <CardBlog />
+        <CardBlog />
+        <CardBlog />
+        <CardBlog />
+        <CardBlog />
+        <CardBlog />
+        <CardBlog />
+        <CardBlog />
       </div>
-    </div>
+    </>
   );
 };
 
