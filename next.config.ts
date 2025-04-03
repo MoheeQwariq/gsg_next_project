aa0a54916142be8572543e3e75a7bfcaeb660707
Mohee Qwariq <143301303+MoheeQwariq@users.noreply.github.com>
2025-04-03 15:03:33 +0800

    Revert "Admin (#17)" (#18)
    This reverts commit a12f26bb92d9490f5a06b69408029961304aad84.

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["example.com"],
-  },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, 
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, DELETE, OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
