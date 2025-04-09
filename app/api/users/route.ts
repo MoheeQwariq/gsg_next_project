74b7085019f429c47224443ec512ae68904f7186
Lama Emad Ali <lama2015678@gmail.com>
2025-04-09 17:53:48 +0200

    Add profile section APIs: create, read, update, and delete with Cloudinary support

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getAllUsers } from "@/services/users.service";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(request: NextRequest) {
     const users = getAllUsers();
     return NextResponse.json(users, { status: 200 });
