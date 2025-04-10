86a600786b6a56c1f854229279f39fb948bcf6d5
MoheeQwariq <moheedeab16@gmail.com>
2025-04-10 09:24:24 +0200

    add blogs

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,22 +1,69 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getAllPosts, deletePost } from "@/services/stories.service";
+import sqlite3 from "better-sqlite3";
+import jwt from "jsonwebtoken";
+import { User } from "@/types/user";
+import { Post } from "@/types/post";
 
-export async function GET(request: NextRequest) {
-  const params = request.nextUrl.searchParams;
-  const type = params.get("type");
-  let posts = getAllPosts();
-  if (type) {
-    posts = posts.filter((post) => post.category === type);
+const db = sqlite3("stories.db");
+const JWT_SECRET = process.env.JWT_SECRET!;
+
+
+export async function POST(req: NextRequest) {
+  const authHeader = req.headers.get("authorization");
+
+  if (!authHeader) {
+    return NextResponse.json({ message: "التوكن مفقود أو غير صالح" }, { status: 401 });
   }
 
-  return NextResponse.json(posts, { status: 200 });
-}
-export async function DELETE(request: NextRequest) {
-  const { id } = await request.json();
-  const isDeleted = await deletePost(id);
-  if (isDeleted) {
-    return NextResponse.json({ message: "Post deleted successfully" });
-  } else {
-    return NextResponse.json({ message: "Failed to delete post" }, { status: 400 });
+  const token = authHeader;
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as { email: string };
+
+    const user = db
+      .prepare("SELECT * FROM users WHERE email = ?")
+      .get(decoded.email) as User | undefined;
+
+    if (!user) {
+      return NextResponse.json({ message: "المستخدم غير موجود" }, { status: 404 });
+    }
+
+    const body = await req.json();
+
+    const {
+      id,
+      title,
+      content,
+      image = null,
+      category,
+      createdAt,
+    } = body as Omit<Post, "author" | "authorEmail" | "userId">;
+
+    if (!id || !title || !content || !category || !createdAt) {
+      return NextResponse.json({ message: "بيانات غير مكتملة" }, { status: 400 });
+    }
+
+    const insertPost = db.prepare(`
+      INSERT INTO posts (id, title, content, author, authorEmail, image, category, createdAt, userId)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `);
+
+    insertPost.run(
+      id,
+      title,
+      content,
+      user.name,
+      user.email,
+      image,
+      category,
+      createdAt,
+      user.id
+    );
+
+    return NextResponse.json({ message: "تم إنشاء المنشور بنجاح" }, { status: 201 });
+
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ message: "توكن غير صالح" }, { status: 401 });
   }
 }
